6c3a210a670e9f0cbb7e5ecb9be82f9a959c9445
Alejandro-De-La-Torre-Garcia567 <id00931782@usal.es>
2026-09-22 19:18:20 +0200

    calculo de edad segun año introducido a traves de la clase Scanner

--- src/App.java
+++ src/App.java
@@ -1,8 +1,17 @@
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
         
-            
+        System.out.print("Introduzca su año de nacimiento:");    
 
+        Scanner number = new Scanner(System.in);
+        int year =number.nextInt();
 
+        int age=2026-year;
+        System.out.println(age);
+
+        scanner.close();
     }
 }
