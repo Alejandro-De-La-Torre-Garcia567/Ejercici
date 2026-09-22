65f2f3c8d7eb8e1f7bd63787efac3678e5e63a15
Alejandro-De-La-Torre-Garcia567 <id00931782@usal.es>
2026-09-22 19:29:35 +0200

    mejora

--- src/App.java
+++ src/App.java
@@ -7,9 +7,37 @@ public class App {
         System.out.print("Introduzca su año de nacimiento:");    
 
         Scanner number = new Scanner(System.in);
+
         int year =number.nextInt();
 
-        int age=2026-year;
+
+        System.out.print("Introduzca el año actual:");
+
+
+        int currentYear = number.nextInt();
+
+        int age=currentYear-year;
+
+    if(year <0){
+
+
+            System.out.println("Error, el año de nacimiento no puede ser negativo");
+            return;
+
+    }
+
+    if(currentYear <0){
+
+            System.out.println("Error, el año actual no puede ser negativo");
+            return;
+    }
+
+    if(year>currentYear){
+
+            System.out.println("Error, el año de nacimiento no puede ser mayor que el año actual");
+            return;
+    }
+
         System.out.println("Su edad es: " + age + " años");
 
         number.close();
